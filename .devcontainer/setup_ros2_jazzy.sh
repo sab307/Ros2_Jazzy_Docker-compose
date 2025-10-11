b18b01781e6dd4eae211cddcfd4e55a8dccf3cf9
Sabeeh Musharraf <sabeeh.musharraf@hotmail.com>
2025-10-11 23:24:54 +0200

    Added devcontainer folder having zenoh docker service with health check

--- .devcontainer/setup_ros2_jazzy.sh
+++ .devcontainer/setup_ros2_jazzy.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 source /opt/ros/jazzy/setup.bash
+sudo apt-get update
+sudo apt-get install -y babeltrace ros-jazzy-ros2trace ros-jazzy-tracetools-analysis
+ros2 run tracetools status
+rosdep update
+rosdep install -i -y --from-paths src --rosdistro jazzy --skip-keys test_tracetools
